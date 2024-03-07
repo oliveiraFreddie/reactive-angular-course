457babded539aec9556abda2f9c5b14f2bfa9d8c
oliveiraFreddie <freddie.oliveira95@gmail.com>
2024-03-07 16:50:15 +0000

    first-9-lessons

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CoursesService {
+  constructor(private http: HttpClient) { }
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http
+      .get<Course[]>("/api/courses")
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
+  }
+}
