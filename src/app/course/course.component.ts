d44b7eacbf07479d687b60335b6eb7deceb92522
oliveiraFreddie <freddie.oliveira95@gmail.com>
2024-03-15 15:31:19 +0000

    37-done

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
 import {
   debounceTime,
   distinctUntilChanged,
@@ -13,8 +13,14 @@ import {
   withLatestFrom,
   concatAll, shareReplay, catchError
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
-import {Lesson} from '../model/lesson';
+import { merge, fromEvent, Observable, concat, throwError, combineLatest } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
+
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 
 @Component({
@@ -24,19 +30,39 @@ import {Lesson} from '../model/lesson';
 })
 export class CourseComponent implements OnInit {
 
-  course: Course;
+  data$: Observable<CourseData>
 
-  lessons: Lesson[];
-
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private coursesService: CoursesService
+  ) {
 
 
   }
 
   ngOnInit() {
+    const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"))
 
+    const course$ = this.coursesService.loadCourseById(courseId)
+      .pipe(
+        startWith(null)
+      );
 
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId)
+      .pipe(
+        startWith([])
+      );
 
+    this.data$ = combineLatest([course$, lessons$])
+      .pipe(
+        map(([course, lessons]) => {
+          return {
+            course,
+            lessons
+          }
+        }),
+        tap(console.log)
+      )
   }
 
 
